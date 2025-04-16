5f4b6b859668ec9c3f044d6d3de8c7d0fc2a9a73
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-16 17:24:27 +0100

    feat: Ajouter le champe de 'roles', 'birth_date' dans la migration users

--- Projet/Backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ Projet/Backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('roles',['Enseignant(e)', 'Secritaire','Comptable']);
+            $table->date("birth_date");
             $table->rememberToken();
             $table->timestamps();
         });
