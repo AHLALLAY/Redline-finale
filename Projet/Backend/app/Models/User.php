6f25cada0fd34d42df9c3958637acbf7853ca949
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-17 00:08:37 +0100

    feat: Ajouter l'inmplementation de JWTSubject

--- Projet/Backend/app/Models/User.php
+++ Projet/Backend/app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -15,5 +16,6 @@ class User extends Authenticatable
     protected $hidden = ['password','remember_token',];
     protected function casts(): array { return ['email_verified_at' => 'datetime','password' => 'hashed',]; }
 
-    
+    public function getJWTIdentifier() { return $this->getKey(); }
+    public function getJWTCustomClaims() { return []; }
 }
