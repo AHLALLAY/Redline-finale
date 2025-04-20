8742d3f7feefc80ed5b7059acf0f1bebd7ff3a4c
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-20 16:16:27 +0100

    feat: Implementer la méthode 'LoginStudent'

--- Projet/BackEnd/app/Http/Controllers/Api/AuthController.php
+++ Projet/BackEnd/app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginStaffRequest;
+use App\Http\Requests\LoginStudentRequest;
 use App\Services\AuthService;
 use Illuminate\Validation\ValidationException;
 
@@ -21,6 +22,31 @@ class AuthController extends Controller
         try {
             $validated_data = $loginStudentRequest->validated();
             $data = $this->authService->LoginStaff($validated_data);
+
+            return response()->json([
+                'message' => 'Connexion réussie',
+                'data' => $data
+            ], 200);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Erreur de validation',
+                'errors' => $e->errors(),
+                'status' => 'error'
+            ], 422);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Échec de la connexion',
+                'error' => $e->getMessage(),
+                'status' => 'error'
+            ], 500);
+        }
+    }
+
+    public function LoginStudent(LoginStudentRequest $loginStudentRequest)
+    {
+        try {
+            $validated_data = $loginStudentRequest->validated();
+            $data = $this->authService->LoginStudent($validated_data);
 
             return response()->json([
                 'message' => 'Connexion réussie',
