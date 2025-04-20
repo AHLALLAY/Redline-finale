584787f4fbe2e49875a65255a8403d8fcf647da6
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-20 14:29:55 +0100

    feat: Ajouter les méthode d'authentification dans l'interface 'AuthInterface'

--- Projet/BackEnd/app/Interfaces/AuthInterface.php
+++ Projet/BackEnd/app/Interfaces/AuthInterface.php
@@ -4,5 +4,14 @@ namespace App\Interfaces;
 
 interface AuthInterface
 {
-    
+    // staff
+    public function RegisterStaff($dataStaff);
+    public function LoginStaff($identStaff);
+
+    // student
+    public function RegisterStudent($dataStudent);
+    public function LoginStudent($identStudent);
+
+    // commun
+    public function Logout();
 }
