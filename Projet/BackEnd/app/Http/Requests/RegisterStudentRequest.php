8ed1059ac890d04f70e49dbe1fd94193cca20237
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-20 19:57:18 +0100

    fix: Corrige la règle de validation pour l'utilisateur student

--- Projet/BackEnd/app/Http/Requests/RegisterStudentRequest.php
+++ Projet/BackEnd/app/Http/Requests/RegisterStudentRequest.php
@@ -23,7 +23,7 @@ class RegisterStudentRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:50'],
-            'email' => ['required', 'string', 'email', 'max:70', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:70', 'unique:students'],
             'password' => ['required', 'string', 'min:8'],
             'birth_date' => ['required', 'date', 'before:-6 years'],
             'birth_place' => ['required', 'string', 'max:50'],
