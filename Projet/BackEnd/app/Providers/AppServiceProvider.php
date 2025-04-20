1657adfb5ae7ff43fddb6e890865a535853b5700
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-20 21:06:37 +0100

    feat: Ajouter la liaison entre AccountantInterface et AccountantRepository

--- Projet/BackEnd/app/Providers/AppServiceProvider.php
+++ Projet/BackEnd/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Interfaces\AccountantInterface;
 use App\Interfaces\AuthInterface;
+use App\Repositories\AccountantRepository;
 use App\Repositories\AuthRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(AuthInterface::class, AuthRepository::class);
+        $this->app->bind(AccountantInterface::class, AccountantRepository::class);
     }
 
     /**
