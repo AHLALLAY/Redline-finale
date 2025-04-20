df25fd467b3d9649a82ec639c997f5887750f55b
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-20 18:01:56 +0100

    feat: Etend le schéma de la table users avec nouveaux champs

--- Projet/BackEnd/database/migrations/0001_01_01_000001_create_users_table.php
+++ Projet/BackEnd/database/migrations/0001_01_01_000001_create_users_table.php
@@ -14,10 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('cin');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['Enseignant', 'Comptable', 'Secrétaire', 'Admin']);
+            $table->date('birth_date');
+            $table->string('phone')->nullable();
+            $table->string('last_deplomat');
+            $table->date('obtained_at');
             $table->timestamps();
         });
 
