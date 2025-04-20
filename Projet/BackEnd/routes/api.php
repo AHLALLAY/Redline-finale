22c239365c56d75da8b0dd749ad7d5e575102566
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-20 16:31:21 +0100

    feat: Ajouter les routes vers les méthode de AuthController

--- Projet/BackEnd/routes/api.php
+++ Projet/BackEnd/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(AuthController::class)->group(function() {
+    Route::post('/login/staff','LoginStaff');
+    Route::post('/login/student','LoginStudent');
+    Route::post('/register/staff','RegisterStaff');
+    Route::post('/register/student','RegisterStudent');
+    Route::post('/logout','Logout');
+});
