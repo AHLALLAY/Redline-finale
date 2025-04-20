6837793940d9afe10259700590e5f68233e98cdb
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-20 16:14:25 +0100

    feat: Ajouter le construct du controller 'AuthController'

--- Projet/BackEnd/app/Http/Controllers/Api/AuthController.php
+++ Projet/BackEnd/app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Services\AuthService;
 
 class AuthController extends Controller
 {
-    //
+    protected $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+    
 }
