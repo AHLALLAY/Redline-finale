857eddf41323793c7c3442ca554515fc9e0b848d
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-22 16:31:30 +0100

    feat: Ajouter une protection pour le tableau de board d'admin

--- Projet/FrontEnd/src/main.jsx
+++ Projet/FrontEnd/src/main.jsx
@@ -5,13 +5,27 @@ import App from './App.jsx'
 import LoginStaff from './views/auth/LoginStaff.jsx'
 import DashboardAdmin from './views/admin/DashboardAdmin.jsx'
 
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem('authToken');
+  return token ? children : <Navigate to="/login/staff" replace />;
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login/staff" element={<LoginStaff />} />
-        <Route path="/dashboard" element={<DashboardAdmin />} />
+        <Route path="/admin/dashboard" element={
+            <ProtectedRoute>
+              <DashboardAdmin />
+            </ProtectedRoute>
+          }
+        />
+
+        
       </Routes>
     </BrowserRouter>
   </StrictMode>
