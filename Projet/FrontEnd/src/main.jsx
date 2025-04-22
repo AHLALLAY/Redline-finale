dc24cc6290161acdaa8102fceb981290b2fd5630
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-22 15:23:39 +0100

    feat: Ajouter le Route vers le composant 'DashboardAdmin.jsx'

--- Projet/FrontEnd/src/main.jsx
+++ Projet/FrontEnd/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import LoginStaff from './views/auth/LoginStaff.jsx'
+import DashboardAdmin from './views/admin/DashboardAdmin.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login/staff" element={<LoginStaff />} />
+        <Route path="/dashboard" element={<DashboardAdmin />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
