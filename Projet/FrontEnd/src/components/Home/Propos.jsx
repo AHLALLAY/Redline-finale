a85526353e028b7a4a0f2eeb0fa1d8a5869c3176
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-22 11:56:31 +0100

    feat: Ajouter le composant 'Propos.jsx'

---
+++ Projet/FrontEnd/src/components/Home/Propos.jsx
@@ -0,0 +1,34 @@
+function Propos() {
+    return (
+        <section>
+            <div>
+                <h2>Notre Ecole</h2>
+                <div>
+                    {/* qui est nos */}
+                    <div>
+                        <div>
+                            <h4>Qui est nos ?</h4>
+                            <p>OMEGA est une école moderne qui forme les leaders de demain avec une pédagogie innovante.</p>
+                        </div>
+                        <div>
+                            <h4>Nos principe</h4>
+                            <ul>
+                                <li>Enseignants qualifiés</li>
+                                <li>Environnement stimulant</li>
+                                <li>Programme complet</li>
+                                <li>Infrastructure moderne</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    {/* image */}
+                    <div>
+                        <img src="/img/2.jpg" alt="Elèves en classe" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Propos;
