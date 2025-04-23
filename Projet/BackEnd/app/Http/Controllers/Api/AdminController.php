f2794d9eaff842fb6ec07ec6503d1d9506cceee3
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-23 13:59:31 +0100

    feat: Implementer la méthode 'DisplayStudent' dans 'AdminController'

--- Projet/BackEnd/app/Http/Controllers/Api/AdminController.php
+++ Projet/BackEnd/app/Http/Controllers/Api/AdminController.php
@@ -21,7 +21,30 @@ class AdminController extends Controller
             $data = $this->adminService->DisplayStaff();
             if ($data === null) {
                 return response()->json([
-                    "message" => "No user found",
+                    "message" => "No Stuff found",
+                    "status" => "success"
+                ], 200);
+            }
+            return response()->json([
+                "message" => "les donner collecter",
+                "data" => $data,
+                "status" => "success"
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "message" => "Unexpected Error",
+                "error" => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function DisplayStudents()
+    {
+        try {
+            $data = $this->adminService->DisplayStudents();
+            if ($data === null) {
+                return response()->json([
+                    "message" => "No Student found",
                     "status" => "success"
                 ], 200);
             }
