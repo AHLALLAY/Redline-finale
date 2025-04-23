b551ac55bc78f679c12b818648e5a7e889df6995
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-23 11:54:39 +0100

    feat: Ajouter des signature à l'interface 'AdminInterface'

--- Projet/BackEnd/app/Interfaces/AdminInterface.php
+++ Projet/BackEnd/app/Interfaces/AdminInterface.php
@@ -4,5 +4,7 @@ namespace App\Interfaces;
 
 interface AdminInterface
 {
-    //
+    public function DisplayStaff();
+    public function DisplayStudents();
+    public function DisplayAbsences();
 }
