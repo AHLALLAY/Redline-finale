7ed661e925fbbd401bb7994991d93fe46a3fde25
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-23 12:14:10 +0100

    feat: Ajouter l'implementation du méthode 'DisplayStaff' dans le repository

--- Projet/BackEnd/app/Repositories/AdminRepository.php
+++ Projet/BackEnd/app/Repositories/AdminRepository.php
@@ -3,10 +3,16 @@
 
 namespace App\Repositories;
 
+use App\Models\User;
 
 class AdminRepository {
 
     public function DisplayStaff(){
+        try{
+            return User::all();
+        }catch(\Exception $e){
+            throw $e;
+        }
         
     }
     public function DisplayStudents(){
