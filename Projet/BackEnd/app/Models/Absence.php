566617fd1c33f35bda59daf1a70fc100d2e2144c
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-23 14:19:42 +0100

    feat: Ajouter les champs fillable dans le model 'Absence'

--- Projet/BackEnd/app/Models/Absence.php
+++ Projet/BackEnd/app/Models/Absence.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Absence extends Model
 {
-    //
+    protected $fillable = ['student_id', 'status', 'delay', 'date', 'period', 'justification', 'recorded_by'];
 }
