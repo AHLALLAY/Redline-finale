8e6edcc33af8c71850047f60e9053fb3bd5008a2
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-24 16:35:57 +0100

    feat: Remplacer le button de lougout par le composant 'logout'

--- Projet/FrontEnd/src/components/Comptable/Header.jsx
+++ Projet/FrontEnd/src/components/Comptable/Header.jsx
@@ -1,3 +1,5 @@
+import Logout from "../Global/Logout";
+
 const Header = () => {
     return (
       <header className="bg-orange-400 shadow-md">
@@ -12,12 +14,7 @@ const Header = () => {
                 <h2 className="font-semibold">Comptable</h2>
               </div>
             </div>
-            <button className="px-4 py-2 rounded-md bg-red-500 hover:bg-red-600 text-white flex items-center gap-2 transition-colors">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" />
-              </svg>
-              Déconnexion
-            </button>
+            <Logout />
           </div>
         </nav>
       </header>
