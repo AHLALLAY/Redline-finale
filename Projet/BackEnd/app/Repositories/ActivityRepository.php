15ac4995735071c6a65d83069b4ecad979d457a0
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-24 17:12:44 +0100

    feat: Ajouter 'ActivityRepository'

---
+++ Projet/BackEnd/app/Repositories/ActivityRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\ActivitiesInterface;
+
+class ActivityRepository implements ActivitiesInterface{
+
+    public function AssignActivity($activity){
+
+    }
+    public function AddActivityToTextBox($activity){
+
+    }
+}
