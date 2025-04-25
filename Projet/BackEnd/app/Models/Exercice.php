fdd782e44652e74c40085f9d7fcba0ee96757de6
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-25 20:38:37 +0100

    feat: Ajouter la relation entre 'Exercice' et 'user'

--- Projet/BackEnd/app/Models/Exercice.php
+++ Projet/BackEnd/app/Models/Exercice.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Exercice extends Model
 {
     protected $fillable = ['title','description','classe','group','teacher_id','is_done','done_at'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
