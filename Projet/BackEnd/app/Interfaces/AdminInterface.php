f6ffd979cefd7f89af76021f22da9d1be309ec97
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-26 16:24:58 +0100

    feat: Ajouter tous les signature néssessaire dans 'AdminInterface'

--- Projet/BackEnd/app/Interfaces/AdminInterface.php
+++ Projet/BackEnd/app/Interfaces/AdminInterface.php
@@ -4,7 +4,18 @@ namespace App\Interfaces;
 
 interface AdminInterface
 {
+    // staff
+    public function AddStaff($staff);
     public function DisplayStaff();
+    public function SuspendStaff($staff);
+    public function DeleteStaff($staff);
+    public function AssignClasse($classe, $to);
+    public function AssignGarde($time, $to);
+
+    // student
     public function DisplayStudents();
     public function DisplayAbsences();
+
+    // auther
+    public function AddOffre($offre);
 }
