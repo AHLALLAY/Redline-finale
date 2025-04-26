60986a9583c113f986c25e5875fe1bb0078a86ea
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-26 23:18:24 +0100

    feat: Ajouter des champs dans la migration 'garde'

--- Projet/BackEnd/database/migrations/2025_04_26_215624_create_garde_table.php
+++ Projet/BackEnd/database/migrations/2025_04_26_215624_create_garde_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -13,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('garde', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('teacher_id')->constrained('users')->onDelete('casccade');
+            $table->foreignId('teacher_id')->constrained('users')->onDelete('cascade');
+            $table->date('date');
+            $table->time('start_time');
             $table->timestamps();
         });
     }
