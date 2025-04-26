0d2f35d6e68e674b92c01ac6967de89bb40cff20
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-26 18:42:30 +0100

    feat: implémenter les fonctionnalité d'admin dans le controller

--- Projet/BackEnd/app/Http/Controllers/Api/AdminController.php
+++ Projet/BackEnd/app/Http/Controllers/Api/AdminController.php
@@ -4,16 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ClasseRequest;
+use App\Http\Requests\GardeRequest;
+use App\Http\Requests\OffreRequest;
 use App\Services\AdminService;
+use Illuminate\Validation\ValidationException;
 
 class AdminController extends Controller
 {
     protected $adminService;
-
     public function __construct(AdminService $adminService)
     {
         $this->adminService = $adminService;
     }
+
+    // staff
+    public function AddStaff($staffData) {}
 
     public function DisplayStaff()
     {
@@ -22,6 +28,105 @@ class AdminController extends Controller
             if ($data === null) {
                 return response()->json([
                     "message" => "No Stuff found",
+                    "status" => "success"
+                ], 200);
+            }
+            return response()->json([
+                "message" => "les donner collecter",
+                "data" => $data,
+                "status" => "success"
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                "message" => "Unexpected Error",
+                "error" => $e->getMessage()
+            ], 500);
+        }
+    }
+    public function SuspendStaff($staffId)
+    {
+        try {
+            $result = $this->adminService->SuspendStaff($staffId);
+            if ($result) {
+                return response()->json([
+                    'message' => 'Suspend successive',
+                    'status' => 'success'
+                ], 200);
+            }
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+    public function DeleteStaff($staffId)
+    {
+        try {
+            $result = $this->adminService->DeleteStaff($staffId);
+            if ($result) {
+                return response()->json([
+                    'message' => 'delete successive',
+                    'status' => 'success'
+                ], 200);
+            }
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+    public function AddClasse(ClasseRequest $classeRequest)
+    {
+        try {
+            $validated_data = $classeRequest->validated();
+            $this->adminService->AddClasse($validated_data);
+            return response()->json([
+                'message' => 'Class Added',
+                'data' => $validated_data,
+                'status' => 'success'
+            ], 201);
+        } catch (ValidationException $e) {
+            return  response()->json([
+                'message' => 'Erreur lors la validation',
+                'error' => $e->errors()
+            ], 422);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Unexpected Error',
+                'error' => $e->getMessage(),
+                'status' => 'error'
+            ], 500);
+        }
+    }
+    public function AddGarde(GardeRequest $gardeRequest)
+    {
+        try {
+            $validated_data = $gardeRequest->validated();
+            $this->adminService->AddGarde($validated_data);
+            return response()->json([
+                'message' => 'Gard Added',
+                'data' => $validated_data,
+                'status' => 'success'
+            ], 201);
+        } catch (ValidationException $e) {
+            return  response()->json([
+                'message' => 'Erreur lors la validation',
+                'error' => $e->errors()
+            ], 422);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Unexpected Error',
+                'error' => $e->getMessage(),
+                'status' => 'error'
+            ], 500);
+        }
+    }
+
+
+    // student
+    public function DisplayAbsences()
+    {
+        try {
+            $data = $this->adminService->DisplayAbsences();
+            if ($data === null) {
+                return response()->json([
+                    "message" => "No Student found",
                     "status" => "success"
                 ], 200);
             }
@@ -57,6 +162,32 @@ class AdminController extends Controller
             return response()->json([
                 "message" => "Unexpected Error",
                 "error" => $e->getMessage()
+            ], 500);
+        }
+    }
+
+
+    // auther
+    public function AddOffre(OffreRequest $offreRequest)
+    {
+        try {
+            $validated_data = $offreRequest->validated();
+            $this->adminService->AddGarde($validated_data);
+            return response()->json([
+                'message' => 'Offre Added',
+                'data' => $validated_data,
+                'status' => 'success'
+            ], 201);
+        } catch (ValidationException $e) {
+            return  response()->json([
+                'message' => 'Erreur lors la validation',
+                'error' => $e->errors()
+            ], 422);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Unexpected Error',
+                'error' => $e->getMessage(),
+                'status' => 'error'
             ], 500);
         }
     }
