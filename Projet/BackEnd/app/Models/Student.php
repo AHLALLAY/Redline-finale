a1c04a207a8c15df123c28504f65c551b76a31b4
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-27 22:38:28 +0100

    refact: Remplacer return direct par switch case

--- Projet/BackEnd/app/Models/Student.php
+++ Projet/BackEnd/app/Models/Student.php
@@ -6,17 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    protected $fillable = ['name', 'email', 'password', 'birth_date', 'birth_place', 'gender', 'level', 'goupe', 'parent', 'cin', 'address', 'phone'];
+    protected $fillable = ['name', 'email', 'password', 'birth_date', 'birth_place', 'gender', 'level', 'group', 'parent', 'cin', 'address', 'phone'];
 
-    public static function getLevelSlugs()
+    public static function getLevelSlugs($level)
     {
-        return [
-            '1ere-annee' => '1ére année',
-            '2eme-annee' => '2ème année',
-            '3eme-annee' => '3ème année',
-            '4eme-annee' => '4ème année',
-            '5eme-annee' => '5ème année',
-            '6eme-annee' => '6ème année',
-        ];
+        switch ($level) {
+            case '1ére année':
+                return 'fst_year';
+                break;
+            case '2ème année':
+                return 'scd_year';
+                break;
+            case '3ème année':
+                return 'trd_year';
+                break;
+            case '4ème année':
+                return 'frh_year';
+                break;
+            case '5ème année':
+                return 'fth_year';
+                break;
+            case '6ème année':
+                return 'sxh_year';
+                break;
+            default:
+                return 'invalide';
+                break;
+        }
     }
 }
