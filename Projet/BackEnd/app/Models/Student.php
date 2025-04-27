1907640ed580dc4a8d2516f4a84ba79c5a73ac7d
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-27 21:25:22 +0100

    feat: Ajouter les slugs pour 'level'

--- Projet/BackEnd/app/Models/Student.php
+++ Projet/BackEnd/app/Models/Student.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    protected $fillable = ['name', 'email', 'password', 'birth_date', 'birth_place', 'gender', 'level', 'goupe', 'parent', 'cin', 'address','phone'];
+    protected $fillable = ['name', 'email', 'password', 'birth_date', 'birth_place', 'gender', 'level', 'goupe', 'parent', 'cin', 'address', 'phone'];
+
+    public static function getLevelSlugs()
+    {
+        return [
+            '1ere-annee' => '1ére année',
+            '2eme-annee' => '2ème année',
+            '3eme-annee' => '3ème année',
+            '4eme-annee' => '4ème année',
+            '5eme-annee' => '5ème année',
+            '6eme-annee' => '6ème année',
+        ];
+    }
 }
