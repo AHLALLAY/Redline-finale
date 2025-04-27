ecc48e64599abea6d5f207c1f74d377115b50fb9
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-27 22:36:12 +0100

    refact: Simplifier le seeder

--- Projet/BackEnd/database/seeders/StudentsSeeder.php
+++ Projet/BackEnd/database/seeders/StudentsSeeder.php
@@ -12,23 +12,27 @@ class StudentsSeeder extends Seeder
     public function run(): void
     {
         $levels = ['1ére année', '2ème année', '3ème année', '4ème année', '5ème année', '6ème année'];
-        $gender = ['Masculin', 'Féminin'];
+        $groups = ['A', 'B', 'C', 'D'];
+        $studentsPerGroup = 22;
+        $totalStudents = count($levels) * count($groups) * $studentsPerGroup; // 6*4*22 = 528
 
-        for ($i = 1; $i <= 246; $i++) {
-            $levelIndex = ($i - 1) % 6;
-            $genderIndex = $i % 2;
+        for ($i = 1; $i <= $totalStudents; $i++) {
+            $levelIndex = (int) (($i - 1) / (count($groups) * $studentsPerGroup));
+            $groupIndex = (int) ((($i - 1) % (count($groups) * $studentsPerGroup)) / $studentsPerGroup);
+            $studentInGroup = ($i - 1) % $studentsPerGroup + 1;
 
             DB::table('students')->insert([
                 'name' => 'Élève ' . $i,
-                'email' => 'eleve' . $i . '@example.com',
-                'password' => Hash::make('password'),
+                'email' => 'eleve' . $i . '@gmail.com',
+                'password' => Hash::make('123456789'),
                 'birth_date' => Carbon::now()->subYears(6 + $levelIndex)->subDays(rand(1, 365))->format('Y-m-d'),
                 'birth_place' => 'Ville ' . rand(1, 10),
-                'gender' => $gender[$genderIndex],
+                'gender' => ($i % 2) ? 'Masculin' : 'Féminin',
                 'level' => $levels[$levelIndex],
-                'parent' => 'Parent d\'Élève ' . $i,
+                'group' => $groups[$groupIndex],
+                'parent' => 'Parent ' . $i,
                 'cin' => 'S' . str_pad($i, 6, '0', STR_PAD_LEFT),
-                'address' => 'Adresse ' . $i . ', Rue ' . rand(1, 100),
+                'address' => 'Adresse ' . $i,
                 'phone' => '06' . str_pad($i, 8, '0', STR_PAD_LEFT),
                 'created_at' => now(),
                 'updated_at' => now(),
