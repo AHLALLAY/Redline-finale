f8395a20c191efa5044c7c8d60a5106636d4520d
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-28 14:58:39 +0100

    feat: Ajouter la route vers'StaffList'

--- Projet/FrontEnd/src/main.jsx
+++ Projet/FrontEnd/src/main.jsx
@@ -11,6 +11,7 @@ import ProtecteAdminDashboard from './components/Protectors/ProtecteAdminDashboa
 import ProtecteEnseignantDashboard from './components/Protectors/ProtecteEnseignantDashboard.jsx'
 import ProtecteComptableDashboard from './components/Protectors/ProtecteComptableDashboard.jsx'
 import Transaction from './views/comptable/Transaction.jsx'
+import StaffListe from './views/admin/StaffListe.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -21,9 +22,18 @@ createRoot(document.getElementById('root')).render(
         <Route path="/admin/dashboard" element={
             <ProtecteAdminDashboard>
               <DashboardAdmin />
+              
             </ProtecteAdminDashboard>
           }
         />
+        
+        <Route path="/admin/staff" element={
+            <ProtecteAdminDashboard>
+              <StaffListe />
+            </ProtecteAdminDashboard>
+          }
+        />
+
         <Route path="/enseignant/dashboard" element={
             <ProtecteEnseignantDashboard>
               <DashboardEnseingnant />
