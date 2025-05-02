004055d4ff27a80ae10121be84ce65c4c70625f4
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-05-02 22:43:43 +0100

    feat: ajouter les relation entre 'Garde' et autre models

--- Projet/BackEnd/app/Models/Garde.php
+++ Projet/BackEnd/app/Models/Garde.php
@@ -8,5 +8,6 @@ class Garde extends Model
 {
     protected $fillable = ['teacher_id', 'date','start_time'];
 
-    public function teacher(){ return $this->belongsTo(User::class) ;}
+    public function teacher() { return $this->belongsTo(User::class, 'teacher_id'); }
+    
 }
