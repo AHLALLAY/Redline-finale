d398f92157d55ab348115155e14228b566490917
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-05-02 15:36:55 +0100

    refact: Remplacer 'authToken' par 'token'

--- Projet/FrontEnd/src/components/Protectors/ProtecteAdminDashboard.jsx
+++ Projet/FrontEnd/src/components/Protectors/ProtecteAdminDashboard.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 
 const ProtecteAdminDashboard = ({ children }) => {
-    const token = localStorage.getItem('authToken');
+    const token = localStorage.getItem('token');
     
     if (!token) {
         // Redirige vers /login/staff si non authentifié
