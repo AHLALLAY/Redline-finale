6c98abf37a71e9158af247be645fb8b51b6613d3
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-05-02 22:43:24 +0100

    feat: ajouter les relation entre 'Classe' et autre models

--- Projet/BackEnd/app/Models/Classe.php
+++ Projet/BackEnd/app/Models/Classe.php
@@ -8,5 +8,7 @@ class Classe extends Model
 {
     protected $fillable = ['niveau', 'group', 'teacher_id'];
 
-    public function teacher(){ return $this->belongsToMany(User::class); }
+    public function teacher() { return $this->belongsTo(User::class, 'teacher_id');}
+    public function students() { return $this->hasMany(Student::class); }
+    public function timeTables() { return $this->hasMany(TimeTable::class, 'level', 'niveau')->where('group', $this->group); }
 }
