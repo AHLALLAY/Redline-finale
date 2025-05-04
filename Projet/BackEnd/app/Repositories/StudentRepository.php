4b402b32ab3ae121775fd3ace991eadbddd26d07
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-05-04 15:09:50 +0100

    feat: Ajouter 'StudentRepository' avec l'implementation des méthode de 'StudentInteface'

---
+++ Projet/BackEnd/app/Repositories/StudentRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\StudentInterface;
+
+class StudentRepository implements StudentInterface {
+    public function getGrades(int $studentId)
+    {
+        
+    }
+
+    public function getExercises(int $classId)
+    {
+        
+    }
+}
