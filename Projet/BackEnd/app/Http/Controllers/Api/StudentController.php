5f3c1af5a76dd20a4fc6414bbfe1c2cbf764cb50
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-05-04 15:28:42 +0100

    feat: Ajouter 'StudentController' et implémenter 'getGrade' et 'getExrcise'

--- Projet/BackEnd/app/Http/Controllers/Api/StudentController.php
+++ Projet/BackEnd/app/Http/Controllers/Api/StudentController.php
@@ -4,8 +4,47 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Services\StudentService;
+use Symfony\Component\CssSelector\Node\FunctionNode;
 
 class StudentController extends Controller
 {
-    
+    protected $studentService;
+
+    public function __construct(StudentService $studentService){ $this->studentService = $studentService; }
+
+    public function getGrades($studentId){
+        try{
+            $grades = $this->studentService->getGrades($studentId);
+
+            return response()->json([
+                'message' => 'data found',
+                'grade' => $grades,
+                'status' => 'success'
+            ], 200);
+        }catch(\Exception $e){
+            return response()->json([
+                'message' => 'Unexpected Error',
+                'status' => 'failed'
+            ], 500);
+        }
+
+    }
+
+    public function getExercise($classId){
+        try{
+            $exercises = $this->studentService->getGrades($classId);
+
+            return response()->json([
+                'message' => 'data found',
+                'grade' => $exercises,
+                'status' => 'success'
+            ], 200);
+        }catch(\Exception $e){
+            return response()->json([
+                'message' => 'Unexpected Error',
+                'status' => 'failed'
+            ], 500);
+        }
+    }
 }
