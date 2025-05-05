2511a5a23ea2b988d99c9a7b8901622526c3171f
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-05-05 11:30:29 +0100

    feat: Ajouter la balise 'main'

--- Projet/FrontEnd/src/App.jsx
+++ Projet/FrontEnd/src/App.jsx
@@ -1,16 +1,18 @@
-import Carousel from './components/Home/Carousel'
-import Footer from './components/Home/Footer'
 import Header from './components/Home/Header'
+import Carousel from './components/Home/Carousel'
 import Offres from './components/Home/Offres'
 import Propos from './components/Home/Propos'
+import Footer from './components/Home/Footer'
 
 function App() {
   return (
     <div className="bg-orange-50">
       <Header />
-      <Carousel />
-      <Offres />
-      <Propos />
+      <main className="pt-16">
+        <Carousel />
+        <Offres />
+        <Propos />
+      </main>
       <Footer />
     </div>
   )
