e8c67d0626b278ac48aca5298ef7993503872448
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-05-12 16:41:43 +0100

    feat: Annuler le champs 'recorded_by'

--- Projet/BackEnd/app/Models/Transaction.php
+++ Projet/BackEnd/app/Models/Transaction.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    protected $fillable = [ 'description', 'amount', 'type', 'reference_number', 'entity_name', 'entity_type', 'recorded_by' ];
+    protected $fillable = [ 'description', 'amount', 'type', 'reference_number', 'entity_name', 'entity_type'];
 
     public function recorder() { return $this->belongsTo(User::class, 'recorded_by'); }
 }
